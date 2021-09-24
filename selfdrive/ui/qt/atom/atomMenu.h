6a008d141f1e07c39739da3bf91332ce5a29bb61
hikee9123 <hikee9123@gmail.com>
2021-09-24 22:40:02 +0900

    fix

--- selfdrive/ui/qt/atom/atomMenu.h
+++ selfdrive/ui/qt/atom/atomMenu.h
@@ -29,7 +29,7 @@ private:
   void  fill_rect(NVGcontext *vg, const Rect &r, const NVGcolor *color, const NVGpaint *paint, float radius);
   void  draw_text(const UIState *s, float x, float y, const char *string, float size, NVGcolor color, const char *font_name = 0);
 
-protected:
+public:
   void ui_draw( UIState *s, int w, int h );
 
 };
