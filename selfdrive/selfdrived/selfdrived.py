31449221d3fa7bc6e040ebe4ae547ff8f1b26bd9
bhc-suhwoo <bhc@suhwoo.com>
2025-05-06 12:01:35 +0900

    update enable

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -182,11 +182,12 @@ class SelfdriveD:
           # body always wants to enable
           self.events.add(EventName.pcmEnable)
 
-      # Disable on rising edge of accelerator or brake. Also disable on brake when speed > 0
-      if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or \
-        (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or \
-        (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
-        self.events.add(EventName.pedalPressed)
+      if self.CP.openpilotLongitudinalControl: #custom
+        # Disable on rising edge of accelerator or brake. Also disable on brake when speed > 0
+        if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or \
+          (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or \
+          (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
+          self.events.add(EventName.pedalPressed)
 
     # Create events for temperature, disk space, and memory
     if self.sm['deviceState'].thermalStatus >= ThermalStatus.red:
