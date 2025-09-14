2be44cf3e3e48a6737b5fcd1c1f78421f59eb8e2
bhc-suhwoo <bhc-suhwoo@gmail.com>
2025-09-14 13:29:45 +0900

    fix

--- opendbc_repo/opendbc/safety/modes/hyundai_common.h
+++ opendbc_repo/opendbc/safety/modes/hyundai_common.h
@@ -83,10 +83,6 @@ void hyundai_common_cruise_state_check(const bool cruise_engaged) {
 
     if (!cruise_engaged) {
       controls_allowed = false;
-
-
-      //controls_allowed = true;
-      //print("controls_allowed = false; - hyundai_common_cruise_state_check");
     }
     cruise_engaged_prev = cruise_engaged;
   }
@@ -109,9 +105,7 @@ void hyundai_common_cruise_buttons_check(const int cruise_button, const bool mai
 
     // exit controls on cancel press
     if (cruise_button == HYUNDAI_BTN_CANCEL) {
-      //controls_allowed = false;
-
-      //print("controls_allowed = false; - hyundai_common_cruise_buttons_check");
+      controls_allowed = false;
     }
 
     cruise_button_prev = cruise_button;
