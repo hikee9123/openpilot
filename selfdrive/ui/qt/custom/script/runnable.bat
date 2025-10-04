bd90c3d40fb27402bd75d0086a90fef8173babd3
bhc-suhwoo <bhc@suhwoo.com>
2025-10-04 12:57:47 +0900

    fix

--- selfdrive/ui/qt/custom/script/runnable.bat
+++ selfdrive/ui/qt/custom/script/runnable.bat
@@ -10,28 +10,3 @@ git update-index --chmod=+x load_preset2.sh
 git update-index --chmod=+x panda_flashing.sh
 git update-index --chmod=+x save_preset1.sh
 git update-index --chmod=+x save_preset2.sh
-cd ../../../../system
-git update-index --chmod=+x kupdate.py
-cd ../selfdrive/mapd
-git update-index --chmod=+x config.py
-git update-index --chmod=+x mapd.py
-cd lib
-git update-index --chmod=+x geo.py
-git update-index --chmod=+x helpers.py
-git update-index --chmod=+x NodesData.py
-git update-index --chmod=+x osm.py
-git update-index --chmod=+x Route.py
-git update-index --chmod=+x WayCollection.py
-git update-index --chmod=+x WayRelation.py
-git update-index --chmod=+x WayRelationIndex.py
-cd ../../enavi
-git update-index --chmod=+x navi_external.py
-git update-index --chmod=+x navi_external_udp.py
-cd ../../system/manager
-git update-index --chmod=+x custom_dep.py
-git update-index --chmod=+x local_osm_install.py
-cd ../../opendbc_repo/opendbc/car/hyundai
-git update-index --chmod=+x radar_interface.py
-cd ../../../../selfdrive/controls/lib
-git update-index --chmod=+x lateral_planner.py
-cd ../../..
